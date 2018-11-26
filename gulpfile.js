66ae51240911743a7948b631e1409e3f3a710ce1
smithwebtek <brad@smithwebtek.com>
2018-11-26 12:20:11 -0500

    Code along with tutorial

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
-
+// gulp.task('watch', function () {
+// 	// gulp.watch('app/scss/**/*.scss', ['sass'])
+// 	//https://stackoverflow.com/questions/39665773/gulp-error-watch-task-has-to-be-a-function
+// 	gulp.watch('app/scss/**/*.scss', ['browserSync'], gulp.series('sass'))
+// })
 
 gulp.task('watch', function () {
-	// gulp.watch('app/scss/**/*.scss', ['sass'])
-	//https://stackoverflow.com/questions/39665773/gulp-error-watch-task-has-to-be-a-function
-	gulp.watch('app/scss/**/*.scss', gulp.series('sass'))
-})
+	gulp.watch('app/scss/**/*.scss',
+		gulp.series('sass'),
+		gulp.series('browserSync'))
+});
 
 gulp.task('hello', function (done) {
-	console.log("hello zell")
+	console.log("hello zello")
 	done()
 });
 
@@ -20,8 +25,22 @@ gulp.task('hello', function (done) {
 // 		.pipe(gulp.dest('app/css'))
 // });
 
+// --- OR ----
+// note, this creates a subdirectory under 'app/css/' called /scss
+
 gulp.task('sass', function () {
 	return gulp.src('app/**/*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
 });
+
+gulp.task('browserSync', function () {
+	browserSync.init({
+		server: {
+			baseDir: 'app'
+		}
+	})
+})
